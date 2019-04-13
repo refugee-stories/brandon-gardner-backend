a780d0381c2a035c554e35a129725f620395db51
Brandon G-Home <brandon.gardner2@outlook.com>
2019-04-13 15:27:19 -0500

    Basic router setup finished

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const helmet = require("helmet");
 const cors = require("cors");
 
 const restricted = require("./middleware/restricted");
+const loginRouter = require("./routes/login");
+const storyRouter = require("./routes/stories");
+const adminRouter = require("./routes/admin");
 
 const server = express();
 
@@ -12,6 +15,9 @@ const middleware = [express.json(), helmet(), cors()];
 server.use(middleware);
 
 //Routes Middleware
+server.use("/api/auth/login", loginRouter);
+server.use("api/stories", storyRouter);
+server.use("api/admin", restricted("admin"), adminRouter);
 
 server.get("/", (req, res) => {
   res.sendFile(path.join(__dirname + "/index.html"));
