1a27571be0811be352862fe7411d14197aca28d0
Brandon G-Home <brandon.gardner2@outlook.com>
2019-04-15 15:30:19 -0500

    add done callback to try and fight database locking

--- routes/admin/__tests__/adminStory.js
+++ routes/admin/__tests__/adminStory.js
@@ -4,7 +4,7 @@ const db = require("../../../data/config");
 
 describe("Admin Testing", () => {
   let token;
-  beforeAll(async () => {
+  beforeAll(async done => {
     //Seed the database with test information.
     await db.seed.run();
     //Login for admin token
@@ -15,18 +15,20 @@ describe("Admin Testing", () => {
     expect(res.status).toBe(200);
     expect(res.body.token).toBeTruthy();
     token = res.body.token;
+    done();
   });
 
-  it("Should fetch all pending stories", async () => {
+  it("Should fetch all pending stories", async done => {
     const res = await request(server)
       .get("/api/admin/stories")
       .set("authorization", token);
     expect(res.status).toBe(200);
     expect(Array.isArray(res.body)).toBeTruthy();
     expect(res.body).toHaveLength(3);
+    done();
   });
 
-  it("Should send back 400 if no title,story or highlight", async () => {
+  it("Should send back 400 if no title,story or highlight", async done => {
     const res = await request(server)
       .post("/api/admin/stories/approve/1")
       .set("authorization", token);
@@ -34,9 +36,10 @@ describe("Admin Testing", () => {
     expect(res.body.message).toBe(
       "Please provide a title, story, and highlight to add."
     );
+    done();
   });
 
-  it("Should send back newID & 201 if story is successfully approved.", async () => {
+  it("Should send back newID & 201 if story is successfully approved.", async done => {
     const story = {
       story: "pending1",
       title: "test1",
@@ -56,9 +59,10 @@ describe("Admin Testing", () => {
     );
     expect(filter).toBeTruthy();
     expect(filter).toHaveLength(1);
+    done();
   });
 
-  it("Should return 404 when trying to post a non existant story", async () => {
+  it("Should return 404 when trying to post a non existant story", async done => {
     const story = {
       story: "pending1",
       title: "test1",
@@ -70,35 +74,40 @@ describe("Admin Testing", () => {
       .set("authorization", token);
     expect(res.status).toBe(404);
     expect(res.body.message).toBe("The story could not be found.");
+    done();
   });
 
-  it("Should return 404 for reject if story doesn't exist", async () => {
+  it("Should return 404 for reject if story doesn't exist", async done => {
     const res = await request(server)
       .delete("/api/admin/stories/reject/8")
       .set("authorization", token);
     expect(res.status).toBe(404);
     expect(res.body.message).toBe("Story could not be located.");
+    done();
   });
 
-  it("Should return 200 if story is rejected", async () => {
+  it("Should return 200 if story is rejected", async done => {
     const res = await request(server)
       .delete("/api/admin/stories/reject/2")
       .set("authorization", token);
     expect(res.status).toBe(200);
+    done();
   });
 
-  it("Should return 404 for delete if story doesn't exist", async () => {
+  it("Should return 404 for delete if story doesn't exist", async done => {
     const res = await request(server)
       .delete("/api/admin/stories/delete/11")
       .set("authorization", token);
     expect(res.status).toBe(404);
     expect(res.body.message).toBe("Story could not be located.");
+    done();
   });
 
-  it("Should return 200 if story is deleted", async () => {
+  it("Should return 200 if story is deleted", async done => {
     const res = await request(server)
       .delete("/api/admin/stories/delete/2")
       .set("authorization", token);
     expect(res.status).toBe(200);
+    done();
   });
 });
